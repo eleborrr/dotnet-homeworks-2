976b7fe7047b52e99e946c2a1ec7246e7672f33f
Gleb Petukhov <gleb-petuhov@mail.ru>
2022-10-23 20:30:48 +0300

    added use and let!

--- Homework6/Hw6.Client/Program.fs
+++ Homework6/Hw6.Client/Program.fs
@@ -5,15 +5,15 @@ open System.Threading.Tasks
 
 let fetchAsync =
     async{
-        let client = new HttpClient()
+        use client = new HttpClient()
         while true do
             try
                 let args = Console.ReadLine().Split()
                 if args.Length <> 3 then
                     raise (ArgumentException("wrong arguments length"))
                 let uri = new Uri $"https://localhost:5001/calculate?value1={args[0]}&operation={args[1]}&value2={args[2]}"
-                let res = client.GetStringAsync(uri)
-                printfn $"{res.Result}"
+                let! res = Async.AwaitTask(client.GetStringAsync(uri))
+                printfn $"{res}"
             with
                 | ex -> printfn "%s" (ex.Message);
     }
